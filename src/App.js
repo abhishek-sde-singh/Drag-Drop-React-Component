4e0a9f82856b822c7467332dd04b7ba5e6f99b8c
Abhishek Singh <abhi@Abhisheks-MacBook-Pro.local>
2024-10-13 10:47:19 +0530

    drag and drop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useRef } from "react";
+import { initialData } from "./data";
 
 function App() {
+  const [data, setData] = useState(initialData);
+  const dragItem = useRef();
+  const dragContainer = useRef();
+
+  const handleDrop = (e, container) => {
+    const item = dragItem.current;
+    const source = dragContainer.current;
+
+    setData((prev) => {
+      const newData = { ...prev };
+      newData[source] = newData[source].filter((i) => i !== item);
+      newData[container] = [...newData[container], item];
+      return newData;
+    });
+  };
+
+  const handleDragStart = (e, child, container) => {
+    dragItem.current = child;
+    dragContainer.current = container;
+    e.target.style.opacity = "0.5";
+  };
+
+  const handleDragEnd = (e) => {
+    e.target.style.opacity = "1";
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Drag & Drop</h1>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(3,1fr)",
+          height: "300px",
+          columnGap: "20px",
+        }}
+      >
+        {Object.keys(data).map((container, index) => {
+          return (
+            <div
+              style={{ backgroundColor: "grey" }}
+              key={index}
+              onDrop={(e) => handleDrop(e, container)}
+              onDragOver={(e) => e.preventDefault()}
+            >
+              <h1>{container}</h1>
+              {data[container].map((child, index) => {
+                return (
+                  <div
+                    key={index}
+                    draggable
+                    onDragStart={(e) => handleDragStart(e, child, container)}
+                    onDragEnd={(e) => handleDragEnd(e)}
+                    style={{
+                      height: "20px",
+                      width: "max-content",
+                      backgroundColor: "red",
+                      padding: "6px",
+                      margin: "4px",
+                      cursor: "move",
+                    }}
+                  >
+                    {child}
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,5 @@
+export const initialData = {
+  Todo: ["Learn React", "Learn LLD", "Learn New Things"],
+  InProgress: ["Learning core web vitals", "preparing for interviews"],
+  Completed: ["HTML", "CSS"],
+};
